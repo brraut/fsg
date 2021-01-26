ada3aa696aacfe9a240c18d3e68f0b5f1dbc2d61
brraut <bishowraut@gmail.com>
2021-01-26 15:31:25 +0545

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,132 +1,29 @@
-function Slider1(i, j) {
-  let t1 = gsap.timeline();
-  let newBannerInner = bannerInner[i];
-  let newBannerContent = bannerContent[i];
-  let newBannerImg = bannerImg[i];
+// document.addEventListener("DOMContentLoaded", function () {
+// gsap.registerPlugin(ScrollTrigger);
 
-  t1.to(
-    newBannerInner,
-    {
-      zIndex: j,
-    },
-    0
-  )
-    .fromTo(
-      newBannerContent,
-      {
-        x: -300,
-        opacity: 0,
-        scale: 0.5,
-        ease: "power2.inOut",
-      },
-      {
-        duration: 1,
-        opacity: 1,
-        scale: 1,
-        x: 0,
-      }
-    )
-    .fromTo(
-      newBannerImg,
-      {
-        x: 300,
-        opacity: 0,
-        scale: 0.5,
-        ease: "power2.inOut",
-      },
-      {
-        duration: 1,
-        x: 0,
-        opacity: 1,
-        scale: 1,
-      }
-    )
-    .to(newBannerInner, {
-      duration: 1,
-      delay: 2,
-      rotationX: 50,
-      scale: i * 0.05 + 0.2,
-    })
-    .to(newBannerInner, {
-      duration: 1,
-      y: 200,
-      onComplete: sliderChange1,
-      onCompleteParams: [i, j, t1],
-    });
-}
-
-function Slider2(i, j) {
-  let t2 = gsap.timeline();
-
-  let newBannerInner = bannerInner[i];
-  t2.to(newBannerInner, {
-    zIndex: i,
-    y: 0,
-  })
-
-    .to(newBannerInner, {
-      duration: 3,
-
-      rotationX: 0,
-      scale: 1,
-    })
-    .to(newBannerInner, {
-      delay: 1,
-      onComplete: sliderChange2,
-      onCompleteParams: [i, j, t2],
-    });
-}
-function sliderChange1(i, j, t1) {
-  let k = bannerInner.length;
-
-  i += 1;
-  j -= 1;
-
-  if (i == k) {
-    t1.kill();
-    Slider2(0, k);
-  } else {
-    Slider1(i, j);
-  }
-}
-function sliderChange2(i, j, t2) {
-  let k = bannerInner.length;
-
-  i += 1;
-  j -= 1;
-
-  if (i == k) {
-    t2.kill();
-    for (let q = 0; q < k; q++) {
-      bannerInner[q].style.zIndex = 0;
-    }
-    Slider1(0, k);
-  } else {
-    Slider2(i, j);
-  }
-}
-gsap.registerPlugin(ScrollTrigger);
-
-var t1 = gsap.timeline();
-t1.fromTo(
-  "#fade1",
-  { autoAlpha: 0, y: 90 },
-  { duration: 1.5, autoAlpha: 1, y: 0 }
-);
-
-ScrollTrigger.create({
-  trigger: "#fade1",
-  start: "30% bottom",
-  end: "10% bottom",
-  onEnter: () => {
-    t1.play();
-  },
-  onEnterBack: () => {
-    t1.reverse();
+let t1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#fade1",
+    start: "top bottom-=100px",
+    end: "+=900",
+    scrub: 1,
   },
 });
 
-var t2 = gsap.timeline();
+t1.from("#fade1", { autoAlpha: 0, y: 100 }).to("#fade1", {
+  duration: 1.5,
+  autoAlpha: 1,
+  y: 0,
+});
+
+var t2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".fadeUp1",
+    start: "top bottom",
+    end: "+=600",
+    scrub: 1,
+  },
+});
 t2.fromTo(
   ".fadeUp1",
   { autoAlpha: 0, y: 90 },
@@ -137,19 +34,14 @@ t2.fromTo(
   { duration: 1, autoAlpha: 1, x: 0 }
 );
 
-ScrollTrigger.create({
-  trigger: ".fadeUp1",
-  start: "30% bottom",
-  end: "10% bottom",
-  onEnter: () => {
-    t2.play();
-  },
-  onEnterBack: () => {
-    t2.reverse();
+let t3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".circle-section",
+    start: "top bottom-=300px",
+    end: "+=300",
+    scrub: 1,
   },
 });
-
-var t3 = gsap.timeline();
 t3.to(".circle-item1", { duration: 1.5, x: -90, y: -90 }) //notice that there's no semicolon!
   .to(".circle-item2", { duration: 1.5, x: 90, y: -90 }, "-=1.5")
   .to(".circle-item3", { duration: 1.5, x: -90, y: 90 }, "-=1.5")
@@ -164,18 +56,6 @@ t3.to(".circle-item1", { duration: 1.5, x: -90, y: -90 }) //notice that there's 
     { duration: 0.8, autoAlpha: 1, y: 0 }
   );
 
-ScrollTrigger.create({
-  trigger: ".circle-section",
-  start: "-25% top",
-  end: "300px bottom",
-  onEnter: () => {
-    t3.play();
-  },
-  onEnterBack: () => {
-    t3.reverse();
-  },
-});
-
 const mainBtn = document.getElementById("mainBtn");
 mainBtn.addEventListener("click", () => {
   document.getElementById("navbar").classList.toggle("pressed");
@@ -185,4 +65,18 @@ mainBtn.addEventListener("click", () => {
 const peopleModal = () => {
   const modal = document.getElementById("peopleModal");
   modal.classList.toggle("show");
+};
+
+const myscroll = () => {
+  let height = window.innerHeight;
+  window.scrollTo(500, height);
+};
+
+const myPlay = () => {
+  let iframe = document.querySelector(".playModal");
+  iframe.classList.add("show");
+};
+const myStop = () => {
+  let iframe = document.querySelector(".playModal");
+  iframe.classList.remove("show");
 };
